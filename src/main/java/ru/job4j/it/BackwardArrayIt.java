cfb246185f021987320d3e156d3c57521943fb1c
EliseevSergey <s.eliseev1@gmail.com>
2022-05-12 10:32:35 +0300

    1. Что такое итератор. [#4951]

--- src/main/java/ru/job4j/it/BackwardArrayIt.java
+++ src/main/java/ru/job4j/it/BackwardArrayIt.java
@@ -1,6 +1,6 @@
 package ru.job4j.it;
-
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class BackwardArrayIt implements Iterator<Integer> {
     private final int[] data;
@@ -17,6 +17,9 @@ public class BackwardArrayIt implements Iterator<Integer> {
 
     @Override
     public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
         return data[data.length - 1 - point++];
     }
 }
--- src/test/java/ru/job4j/it/BackwardArrayItTest.java
+++ src/test/java/ru/job4j/it/BackwardArrayItTest.java
@@ -1,21 +1,18 @@
 package ru.job4j.it;
 
-import org.junit.Assert;
 import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertTrue;
 
 public class BackwardArrayItTest {
     @Test
     public void testHasNext() {
-        BackwardArrayIt in = new BackwardArrayIt(new int[] {1, 2, 3, 4});
-        assertThat(in.hasNext(), is(true));
-        assertThat(in.hasNext(), is(true));
-        assertThat(in.hasNext(), is(true));
-        assertThat(in.hasNext(), is(true));
-        assertThat(in.hasNext(), is(true));
-        assertThat(in.hasNext(), is(true));
-        assertThat(in.hasNext(), is(true));
+        BackwardArrayIt in = new BackwardArrayIt(new int[]{1, 2, 3, 4});
+        assertTrue(in.hasNext());
+        assertTrue(in.hasNext());
+        assertTrue(in.hasNext());
+        assertTrue(in.hasNext());
     }
 
     @Test
