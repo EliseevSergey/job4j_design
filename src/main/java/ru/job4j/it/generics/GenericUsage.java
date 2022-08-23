4d9b500c84db6994536c1235e398e377be5baa75
Sergey Eliseev <s.eliseev1@gmail.com>
2022-08-23 08:21:23 +0300

    upd

---
+++ src/main/java/ru/job4j/it/generics/GenericUsage.java
@@ -0,0 +1,41 @@
+package ru.job4j.it.generics;
+
+import java.util.*;
+
+public class GenericUsage {
+    public static void main(String[] args) {
+        List list = new ArrayList<>();
+        list.add("first");
+        list.add("second");
+        list.add("third");
+        String o = (String) list.get(1);
+        list.add(new Person("Zahar", 31, new Date(1988 - 05 - 12)));
+        System.out.println(list.size());
+//        for (String one : list) {
+//            System.out.println(one);
+//        }
+        List<Integer> l = List.of(1, 2, 3, 4);
+        new GenericUsage().printRsl(l);
+
+        List<Person> per = List.of(new Person("Ivan", 31, new Date(1988 - 05 - 12)));
+        new GenericUsage().printInfo(per);
+
+        List<Programmer> pro = List.of(new Programmer("Kolya", 28, new Date(1986 - 05 - 12)));
+        new GenericUsage().printInfo(pro);
+    }
+
+    public void printRsl(Collection<?> col) {
+        for (Iterator<?> it = col.iterator(); it.hasNext();) {
+            Object next = it.next();
+            System.out.println(next);
+        }
+    }
+
+    public void printInfo(Collection<? extends Person> col) {
+        for (Iterator<? extends Person> it = col.iterator(); it.hasNext();) {
+            Person next = it.next();
+            System.out.println(next);
+        }
+    }
+}
+
