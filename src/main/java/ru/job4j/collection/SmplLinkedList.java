df6ce7041301ec024cfb9ff288315b35748059ba
Sergey Eliseev <s.eliseev1@gmail.com>
2022-09-07 13:42:34 +0300

    upd

--- src/main/java/ru/job4j/collection/SmplLinkedList.java
+++ src/main/java/ru/job4j/collection/SmplLinkedList.java
@@ -1,15 +1,44 @@
 package ru.job4j.collection;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 
-public class SmplLinkedList implements SmplLinkedListIterface {
+public class SmplLinkedList<E> implements SmplLinkedListIterface<E> {
+    transient Node<E> first;
+    transient Node<E> last;
+    transient int size;
+    int modCount;
+
+    private static class Node<E> {
+        E item;
+        Node<E> prev;
+        Node<E> next;
+
+        public Node(Node<E> prev, E element,  Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+
+
     @Override
-    public void add(Object value) {
-
+    public void add(E value) {
+        modCount++;
+        if (size == 0) {
+            Node<E> firstNode = new Node<>(null, value, null);
+            first = firstNode;
+            last = firstNode;
+        } else {
+            Node<E> secondNode = new Node<>(last, value, null);
+            last = secondNode;
+        }
     }
 
     @Override
-    public Object get(int index) {
+    public E get(int index) {
         return null;
     }
 
---
+++ src/test/java/ru/job4j/collection/SmplLinkedListTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.collection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.Iterator;
+
+class SmplLinkedListTest {
+
+    private SmplLinkedList<Integer> list;
+
+    @BeforeEach
+    public void initData() {
+        list = new SmplLinkedList<Integer>();
+        list.add(1);
+        list.add(2);
+    }
+
+    @Test
+    void whenAddAndGet() {
+        list.add(3);
+        list.add(4);
+        assertThat(list.get(0)).isEqualTo(1);
+        assertThat(list.get(1)).isEqualTo(2);
+        assertThat(list.get(2)).isEqualTo(3);
+        assertThat(list.get(3)).isEqualTo(4);
+    }
+
+   /* @Test
+    public static void main(String[] args) {
+      */
+}
