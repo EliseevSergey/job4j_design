dd46c2b3f721f415a4a468369d87c18b36536847
Sergey Eliseev <s.eliseev1@gmail.com>
2022-10-20 18:00:43 +0300

    upd

---
+++ src/main/java/ru/job4j/collection/Hash.java
@@ -0,0 +1,31 @@
+package ru.job4j.collection;
+
+public class Hash {
+    /*public static final int hash(Object key) {
+        int h;
+        return (key == null) ?  0 : (h = key.hashCode()) ^ (h >>> 16);
+    }*/
+
+    public static String binary(int num) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 31; i++) {
+            sb.append(num % 2 == 0 ? 0 : 1);
+            sb.append((i + 1) % 8 == 0 ? " " : "");
+            num /= 2;
+        }
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Hash.binary(1751151001));
+        System.out.println(Hash.binary(15));
+        System.out.println(Hash.binary(1751151001 & 15));
+        System.out.println("line");
+        System.out.println(Hash.binary(255));
+        System.out.println(Hash.binary(255 >> 4));
+        System.out.println(Hash.binary(256));
+        System.out.println(Hash.binary(Integer.MAX_VALUE));
+        System.out.println(Hash.binary(Integer.MAX_VALUE >> 16));
+        System.out.println(Hash.binary(2147483647));
+    }
+}
--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -1,6 +1,10 @@
 package ru.job4j.collection;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 
 public class User {
     private String name;
@@ -13,7 +17,54 @@ public class User {
         this.birthday = birthday;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + "User{" + name + '\'' + ", children=" + children + '}';
+    }
+
     public static void main(String[] args) {
-        User ivan = new User("ivan", 2, );
+        Map<User, Object> map = new HashMap<>();
+        Calendar birthday = Calendar.getInstance();
+
+        User first = new User("Ivan", 2, birthday);
+        int hashCode1 = first.hashCode();
+        int hash1 = hashCode1 ^ (hashCode1 >>> 16);
+        int bucket1 = hash1 & 15;
+
+        User second = new User("Ivan", 2, birthday);
+        int hashCode2 = second.hashCode();
+        int hash2 = hashCode2 ^ (hashCode2 >>> 16);
+        int bucket2 = hash2 & 15;
+
+        map.put(first, new Object());
+        map.put(second, new Object());
+
+        System.out.println(first.equals(second));
+        System.out.println(first.birthday.equals(second.birthday));
+        System.out.println(hashCode1 == hashCode2);
+        System.out.printf(" first - хэшкодКлюча: %s,  хэшМапы(результат работы хэшФункции мапы): %s,  бакет=индекс: %s ", hashCode1, hash1, bucket1);
+        System.out.println("");
+        System.out.printf("second - хэшкодКлюча: %s,  хэшМапы(результат работы хэшФункции мапы): %s,  бакет=индекс: %s ", hashCode2, hash2, bucket2);
+        System.out.println("");
+        for (User usr : map.keySet()) {
+            System.out.println(usr);
+        }
     }
 }
