2330e99128bc0940663434859a2c9cb37ec59648
Sergey Eliseev <s.eliseev1@gmail.com>
2022-11-04 14:38:41 +0300

    8. Реализовать собственную структуру данных - HashMap [#1008 #355951]

---
+++ src/main/java/ru/job4j/tree/Tree.java
@@ -0,0 +1,46 @@
+package ru.job4j.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public interface Tree<E> {
+    boolean add(E parent, E child);
+
+    Optional<Node<E>> findBy(E val);
+
+    class Node<E> {
+        final E val;
+        final List<Node<E>> children = new ArrayList<>();
+
+        public Node(E val) {
+            this.val = val;
+        }
+
+        public E getVal() {
+            return val;
+        }
+
+        public List<Node<E>> getChildren() {
+            return children;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Node<?> node = (Node<?>) o;
+            return Objects.equals(val, node.val) && Objects.equals(children, node.children);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(val, children);
+        }
+    }
+}
