870e3296d4503e62d113edad6cf18a9fa58950f4
Sergey Eliseev <s.eliseev1@gmail.com>
2022-11-15 13:29:28 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,24 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        String[] bufferString = new String[100];
+        int i = 0;
+        for (int x = 1; x < 11; x++) {
+            for (int y = 1; y < 11; y++) {
+                bufferString[i] = x + " * " + y + " = " + x * y;
+                i++;
+            }
+        }
+        try (FileOutputStream fos = new FileOutputStream("result11.txt")) {
+            for (String item : bufferString) {
+                fos.write(item.getBytes());
+                fos.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
