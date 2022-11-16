9a002c7eaaf4d7281564aa59e00cdc37787bc411
Sergey Eliseev <s.eliseev1@gmail.com>
2022-11-16 21:43:25 +0300

    0.3. BufferedReader. [#252489]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -10,36 +10,25 @@ import java.util.stream.Collectors;
 
 public class LogFilter {
     public List<String> filter(String file) {
+        List<String> listString = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader("log.txt"))) {
-            in.lines().forEach(System.out::println);
-
-            List<String> listString = new ArrayList<>();
-
-
-            for (String line = in.readLine(); line != null; line = in.readLine()) {
-                listString.add(line);
-            }
-
-
-            Iterator<String> itr = listString.listIterator();
-            while (itr.hasNext()) {
-                System.out.println(listString.size() + " TRrr " + itr.next());
-            }
-            /*listString.stream()
-                    .filter(item -> item.contains("404"))
-                    .collect(Collectors.toList());*/
-            //listString.stream().forEach(System.out::println);
-
+            listString = in.lines()
+                    .filter(item -> item.contains("404") && item.contains(" "))
+                    .filter(item -> item.lastIndexOf(" ") - item.lastIndexOf("404") == 3)
+                    .collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
+        return listString;
     }
 
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter();
         List<String> log = logFilter.filter("log.txt");
-        System.out.println(log);
+        Iterator<String> itr = log.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
     }
 
 }
