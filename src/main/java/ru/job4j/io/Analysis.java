50955db834e6014b7ee7b6aebc8a48dd2a09d6ce
EliseevSergey <s.eliseev1@gmail.com>
2022-11-23 18:36:37 +0300

    2. Анализ доступности сервера. [#859]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -8,17 +8,15 @@ import java.util.*;
 
 public class Analysis {
     public void unavailable(String source, String target) {
-        List<String> list = new ArrayList<>();
         TreeMap<Date, String> map = new TreeMap<>();
         try (BufferedReader in = new BufferedReader(new FileReader("./data/server.txt"))) {
-            list = in.lines().toList();
+            List<String> list = in.lines().toList();
             for (String str : list) {
                 String[] array = str.split(" ", 2);
                 SimpleDateFormat formater = new SimpleDateFormat("HH:mm:ss");
                 try {
                     Date date = formater.parse(array[1]);
                     map.put(date, array[0]);
-                    System.out.println(date);
                 } catch (ParseException e) {
                     e.printStackTrace();
                 }
@@ -30,16 +28,19 @@ public class Analysis {
                 new BufferedOutputStream(
                         new FileOutputStream(target)
                 ))) {
+            int printSwitcher = 0;
+            var strjnr = new StringJoiner(";");
+            var formater = new SimpleDateFormat("HH:mm:ss");
             for (Date date : map.keySet()) {
-                int switcher = 0;
-                StringJoiner strjnr = new StringJoiner("PPPPPP");
-                if ((map.get(date).equals("400") || map.get(date).equals("500")) && (switcher == 0)) {
-                    SimpleDateFormat formater = new SimpleDateFormat("HH:mm:ss");
-                    strjnr.add(toString());
-                    out.print(strjnr.toString());
-                    switcher = 1;
-                } else {
-                    switcher = 0;
+                if ((map.get(date).equals("400") || map.get(date).equals("500")) && (printSwitcher == 0)) {
+                    strjnr.add(formater.format(date));
+                    printSwitcher = 1;
+                }
+                if (!map.get(date).equals("400") && !map.get(date).equals("500") && (printSwitcher == 1)) {
+                    strjnr.add(formater.format(date));
+                    out.println(strjnr);
+                    strjnr = new StringJoiner(";");
+                    printSwitcher = 0;
                 }
             }
         } catch (Exception e) {
@@ -49,15 +50,6 @@ public class Analysis {
 
     public static void main(String[] args) {
         Analysis fallServer = new Analysis();
-        fallServer.unavailable("./data/server.txt", "./data/unavailableServer.txt");
+        fallServer.unavailable("./data/server.txt", "./data/unavailableServer.csv");
     }
-
-    /*public static void main(String[] args) {
-        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
-            out.println("15:01:30;15:02:32");
-            out.println("15:10:30;23:12:32");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }*/
 }
