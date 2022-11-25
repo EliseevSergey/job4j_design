a34cbbd831ddad8278725475509f800876b32d5a
EliseevSergey <s.eliseev1@gmail.com>
2022-11-25 18:03:56 +0300

    transfert

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -7,19 +7,25 @@ public class Analysis {
     public void unavailable(String source, String target) {
         try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
             BufferedReader in = new BufferedReader(new FileReader(source));
-            int printSwitcher = 0;
-            var strjnr = new StringJoiner(";");
+            boolean printSwitcher = false;
             for (String line = in.readLine(); line != null; line = in.readLine()) {
-                String[] array = line.split(" ");
-                if (((array[0].equals("400") || (array[0].equals("500"))) && (printSwitcher == 0))) {
-                    strjnr.add(array[1]);
-                    printSwitcher = 1;
+                /*String[] array = line.split(" ");
+                if (((array[0].equals("400") || (array[0].equals("500"))) && (!printSwitcher))) {
+                    out.print(array[1]);
+                    printSwitcher = true;
                 }
-                if (!array[0].equals("400") && !array[0].equals("500") && printSwitcher == 1) {
-                    strjnr.add(array[1]);
-                    out.println(strjnr);
-                    strjnr = new StringJoiner(";");
-                    printSwitcher = 0;
+                if (!array[0].equals("400") && !array[0].equals("500") && printSwitcher) {
+                    out.println(String.format(";%s", array[1]));
+                    printSwitcher = false;
+                }*/
+
+                String[] array = line.split(" ");
+                if (((array[0].equals("400") || (array[0].equals("500"))) && (!printSwitcher))) {
+                    out.print(array[1]);
+                    printSwitcher = true;
+                } else {
+                    out.println(String.format(";%s", array[1]));
+                    printSwitcher = false;
                 }
             }
         } catch (IOException e) {
