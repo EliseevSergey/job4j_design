2e05f086f7cbb44d406d0e65ac04dbeec2deefa8
Sergey Eliseev <s.eliseev1@gmail.com>
2022-11-26 06:13:26 +0300

    2. Анализ доступности сервера. [#859 #363561]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -4,18 +4,14 @@ import java.io.*;
 
 public class Analysis {
     public void unavailable(String source, String target) {
-        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
-            BufferedReader in = new BufferedReader(new FileReader(source));
+        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)));
+            BufferedReader in = new BufferedReader(new FileReader(source))) {
             boolean printSwitcher = false;
             for (String line = in.readLine(); line != null; line = in.readLine()) {
                 String[] array = line.split(" ");
-                if (((array[0].equals("400") || (array[0].equals("500"))) && (!printSwitcher))) {
-                    out.print(array[1]);
-                    printSwitcher = true;
-                }
-                if (!array[0].equals("400") && !array[0].equals("500") && printSwitcher) {
-                    out.println(String.format(";%s", array[1]));
-                    printSwitcher = false;
+                if ((array[0].equals("400") || (array[0].equals("500"))) != printSwitcher) {
+                    out.append(array[1]).append(printSwitcher ? System.lineSeparator() : ";");
+                    printSwitcher = !printSwitcher;
                 }
             }
         } catch (IOException e) {
