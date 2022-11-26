1e3ebd95202cec1eb11645830d2de0b2a899277a
Sergey Eliseev <s.eliseev1@gmail.com>
2022-11-26 13:28:19 +0300

    4.0. File [#252491]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -4,16 +4,10 @@ import java.io.File;
 
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("/Users/admin/IdeaProjects");
+        File file = new File("/Users/admin/IdeaProjects/job4j_design/data/server.txt");
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
-        if (!file.isDirectory()) {
-            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
-        }
-        System.out.println(String.format("size : %s", file.getTotalSpace()));
-        for (File subfile : file.listFiles()) {
-            System.out.println(subfile.getAbsoluteFile());
-        }
+        System.out.println(String.format("File name: %s. Size: %s", file.getName(),  file.length()));
     }
 }
