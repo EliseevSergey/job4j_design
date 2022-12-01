8f6fc74fac51ad379169cd5353aa7efdd77fcc55
Sergey Eliseev <s.eliseev1@gmail.com>
2022-12-01 20:58:25 +0300

    4.2. Поиск дубликатов [#315066 #365339]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
-        Path root = Path.of(".");
+        Path root = Path.of("/Users/admin/IdeaProjects");
         var rsl = searchDuplicate(root);
         for (FileProperty fp : rsl.keySet()) {
             System.out.println(String.format("File [%s]. Size [%s] bytes", fp.getName(), fp.getSize()));
@@ -18,9 +20,12 @@ public class DuplicatesFinder {
         }
     }
 
-    public static HashMap<FileProperty, ArrayList<Path>> searchDuplicate(Path root) throws IOException {
+    public static Map<FileProperty, ArrayList<Path>> searchDuplicate(Path root) throws IOException {
         var searcher = new DuplicatesVisitor();
         Files.walkFileTree(root, searcher);
-        return searcher.getMap();
+        return searcher.getMap().entrySet()
+                .stream()
+                .filter(i -> i.getValue().size() > 1)
+                .collect(Collectors.toMap(Entry::getKey, Entry::getValue));
     }
 }
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -8,40 +8,18 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
 
 public class DuplicatesVisitor  extends SimpleFileVisitor<Path> {
-    /*private HashMap<FileProperty, ArrayList<Path>> mainMap = new HashMap<>();
-    private HashMap<FileProperty, ArrayList<Path>> duplicates = new HashMap<>();
-
-    public HashMap<FileProperty, ArrayList<Path>> getDuplicates() {
-        return duplicates;
-    }
-
-    @Override
-    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        FileProperty fp = new FileProperty(file.toFile().getName(), file.toFile().length());
-        ArrayList<Path> paths = new ArrayList<>();
-        paths.add(file);
-        if (!mainMap.containsKey(fp)) {
-            mainMap.put(fp, paths);
-        } else {
-            duplicates.put(fp, mainMap.get(fp));
-            duplicates.get(fp).add(file);
-        }
-        return super.visitFile(file, attrs);
-    }*/
-
-    private HashMap<FileProperty, ArrayList<Path>> map = new HashMap<>();
+    private HashMap<FileProperty, ArrayList<Path>> hashMap = new HashMap<>();
 
     public HashMap<FileProperty, ArrayList<Path>> getMap() {
-        return map;
+        return hashMap;
     }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         FileProperty fp = new FileProperty(file.toFile().getName(), file.toFile().length());
         ArrayList<Path> paths = new ArrayList<>();
-        map.putIfAbsent(fp, paths);
-        map.putIfAbsent(fp, paths).add(file);
-        //map.putIfAbsent(fp, paths);
+        hashMap.putIfAbsent(fp, paths);
+        hashMap.get(fp).add(file);
         return super.visitFile(file, attrs);
     }
 }
