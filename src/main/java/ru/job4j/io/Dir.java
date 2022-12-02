86e9c2675dd05285164f67df55e5a779b60218d1
Sergey Eliseev <s.eliseev1@gmail.com>
2022-12-02 21:38:27 +0300

    transfert

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -4,7 +4,13 @@ import java.io.File;
 
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("/Users/admin/IdeaProjects/job4j_design/data/server.txt");
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is not defined");
+        }
+        File file = new File(args[0]);
+        /**
+         * File file = new File("/Users/admin/IdeaProjects/job4j_design/data/server.txt");
+         */
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
