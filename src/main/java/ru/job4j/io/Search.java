e4c6d340cce2df915cbf7afb4de655bd839e3452
Sergey Eliseev <s.eliseev1@gmail.com>
2022-12-02 21:37:29 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -8,11 +8,26 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Path.of(".");
+        validation(args);
+        Path start = Path.of(args[0]);
+        String extension = args[1];
         search(start, p -> p.toFile()
                 .getName()
-                .endsWith(".txt"))
+                .endsWith(extension))
                 .forEach(System.out::println);
+    }
+
+    public static void validation(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("No arguments are set");
+        }
+        Path start = Path.of(args[0]);
+        if (!start.toFile().exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", start.toAbsolutePath()));
+        }
+        if (args.length == 1) {
+            throw new IllegalArgumentException("File extension is not set up");
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
