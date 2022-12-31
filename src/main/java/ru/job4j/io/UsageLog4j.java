0851c11c6665eaf2023683466f3aab379751e302
Sergey Eliseev <s.eliseev1@gmail.com>
2022-12-31 12:30:01 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -15,5 +15,26 @@ public  static void main(String[] args) {
     LOG.info("info message");
     LOG.warn("warn message");
     LOG.error("error message");
+
+    String name = "Petr Arsentev";
+    int age = 33;
+    LOG.debug("User info : {}, age : {}", name, age);
+    byte b = 127;
+    short s = 32767;
+    int i = 2147483647;
+    long l = 2 ^ 63 - 1;
+    float f = 1.1F;
+    double d = 13D;
+    boolean bln = true;
+    char c = 13;
+    LOG.info("8 primitives. \n"
+            + "byte: {} \n"
+            + "short: {} \n"
+            + "int: {} \n"
+            + "long: {} \n"
+            + "float: {} \n"
+            + "double: {} \n"
+            + "boolean: {} \n"
+            + "char: {} \n", b, s, i, l, f, d, bln, c);
     }
 }
