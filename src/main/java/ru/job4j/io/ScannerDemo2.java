8bd5a44bea5f873df5b51c9a6bb5d500c9d32948
Sergey Eliseev <s.eliseev1@gmail.com>
2023-01-19 13:40:10 +0300

    transfert

--- src/main/java/ru/job4j/io/ScannerDemo2.java
+++ src/main/java/ru/job4j/io/ScannerDemo2.java
@@ -1,15 +1,12 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.Charset;
 import java.util.Scanner;
 
 public class ScannerDemo2 {
     public static void main(String[] args) throws IOException {
-      /*  String instr = "Name: Tom Age: 28 ID: 77";
+        String instr = "Name: Tom Age: 28 ID: 77";
         Scanner conin = new Scanner(instr);
         conin.useDelimiter("7");
         conin.findInLine("Age: ");
@@ -19,22 +16,35 @@ public class ScannerDemo2 {
             System.out.println("fail");
             conin.close();
         }
+
         byte[] barray = "AB".getBytes(Charset.forName("Windows-1252"));
         for (byte b : barray) {
             System.out.println(b);
         }
+
         try (FileReader fileReader = new FileReader("./data/textAB.txt")) {
             StringBuilder sb = new StringBuilder();
             int simbolNum = fileReader.read();
             while (simbolNum != -1) {
-                sb.append(simbolNum);
-                sb.append(System.lineSeparator());
+                sb.append((char)simbolNum);
                 simbolNum = fileReader.read();
             }
-            System.out.println(sb);
+            System.out.println("FileReader with (char) " + System.lineSeparator() + sb);
         } catch (IOException e) {
             e.printStackTrace();
-        }*/
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader("./data/textAB.txt"))) {
+            StringBuilder sb = new StringBuilder();
+            for (String str = br.readLine(); str != null; str = br.readLine()) {
+                sb.append(str);
+                sb.append(System.lineSeparator());
+            }
+            System.out.println("Buffered Reader: " + sb);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         try (FileInputStream fis = new FileInputStream("./data/textAB.txt")) {
             int bb = fis.read();
             StringBuilder sb = new StringBuilder();
@@ -42,16 +52,20 @@ public class ScannerDemo2 {
                 sb.append(bb).append(System.lineSeparator());
                 bb = fis.read();
             }
-            System.out.println(sb);
+            System.out.println("File input Stream" + System.lineSeparator() + sb);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        try (BufferedReader br = new BufferedReader(new FileInputStream("./data/textAB.txt"))) {
-            StringBuilder sbBuf = new StringBuilder();
-            int end = br.readLine();
-            while
-            br.lines();
+        try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream("./data/textAB.txt"))) {
+            StringBuilder sb = new StringBuilder();
+            int end = bis.read();
+            while (end != -1) {
+                sb.append(end).append(System.lineSeparator());
+                end = bis.read();
+            }
+
+            System.out.println("Buffered input: " + System.lineSeparator() + sb);
         }
     }
 }
