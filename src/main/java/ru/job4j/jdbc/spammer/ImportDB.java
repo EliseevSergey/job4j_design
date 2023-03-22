97691bcb7c9609173c35b775d7693f086eebcbba
Sergey Eliseev <s.eliseev1@gmail.com>
2023-03-22 19:03:08 +0300

    0.2. PreparedStatement [#379307]

--- src/main/java/ru/job4j/jdbc/spammer/ImportDB.java
+++ src/main/java/ru/job4j/jdbc/spammer/ImportDB.java
@@ -3,6 +3,7 @@ package ru.job4j.jdbc.spammer;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -20,13 +21,30 @@ public class ImportDB {
         this.dump = dump;
     }
 
+    private static String[] validation(String in) {
+        if (!in.contains(";")) {
+            throw new IllegalArgumentException(String.format("Line %s do not have correct delimiter ;", in));
+        }
+        String[] nameEmail = in.split(";");
+        if (nameEmail.length != 2) {
+            throw new IllegalArgumentException(String.format("Line %s do not have correct shape", in));
+        }
+        if (nameEmail[0].isEmpty() || nameEmail[1].isEmpty()) {
+            throw new IllegalArgumentException(String.format("In line %s , name or email is blank", in));
+        }
+        return nameEmail;
+    }
+
     public List<User> load() throws IOException {
         List<User> users = new ArrayList<>();
-        try (BufferedReader rd = new BufferedReader(new FileReader("./data/spamList.txt"))) {
-             rd.lines()
-                     .forEach(str -> str.split(";", 1));
+        try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
+            List<String> raw = rd.lines().toList();
+            for (String str : raw) {
+                String[] nameEmail = validation(str);
+                users.add(new User(nameEmail[0], nameEmail[1]));
+            }
+            return users;
         }
-        return users;
     }
 
     public void save(List<User> users) throws ClassNotFoundException, SQLException {
@@ -37,9 +55,10 @@ public class ImportDB {
                 cfg.getProperty("jdbc.password")
         )) {
             for (User user : users) {
-                try (PreparedStatement ps = cnt.prepareStatement("insert into users ...")) {
-                    ps.setString(1, user.name);
-                    ps.setString(2, user.email);
+                try (PreparedStatement ps = cnt.prepareStatement(
+                        "insert into users(name, email) values (?, ?)")) {
+                    ps.setString(1, user.getName());
+                    ps.setString(2, user.getEmail());
                     ps.execute();
                 }
             }
@@ -72,7 +91,15 @@ public class ImportDB {
         }
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
+        Properties config = new Properties();
+        try (InputStream in = ImportDB.class.getClassLoader().getResourceAsStream("app.properties")) {
+            config.load(in);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        var importDB = new ImportDB(config, "./data/spamList.txt");
+        List<User> list = importDB.load();
+        importDB.save(list);
     }
 }
