ea4b9100c226dba649c433e679374ec94dca2550
Sergey Eliseev <s.eliseev1@gmail.com>
2023-03-22 19:01:24 +0300

    transfert

--- src/main/java/ru/job4j/io/demo/Split.java
+++ src/main/java/ru/job4j/io/demo/Split.java
@@ -1,6 +1,5 @@
 package ru.job4j.io.demo;
 
-import java.sql.SQLOutput;
 import java.util.Arrays;
 
 import static java.lang.System.out;
@@ -12,23 +11,38 @@ public class Split {
         String str3 = "Petr Arsentev;";
         String str4 = ";parsentev@yandex.ru;";
         String str5 = ";";
-        //String [] rsl = str1.split(";");
-        //Arrays.stream(rsl).forEach(out::println);
+        String str6 = ";;";
+        out.println("this is for Petr Arsentev;parsentev@yandex.ru;");
+        String[] rsl = str1.split(";");
+        Arrays.stream(rsl).forEach(out::println);
 
-        //String [] rsl2 = str3.split(";");
-       // out.println(rsl2.length);
-        //Arrays.stream(rsl2).forEach(out::println);
+        out.println("this is for \"Petr Arsentev;\" ");
+        String[] rsl3 = str3.split(";");
+        out.println(rsl3.length);
+        Arrays.stream(rsl3).forEach(out::println);
 
-        String [] rsl4 = str4.split(";");
+        out.println("this is for \";parsentev@yandex.ru;\" ");
+        String[] rsl4 = str4.split(";");
         out.println(rsl4.length);
         out.println(rsl4[0].isBlank());
         out.println(rsl4[1].isBlank());
         out.println(rsl4[0].length());
 
-        out.println(String.format("this [%s] is 0 when starts with;", rsl4[0]));
+        out.printf("this [%s] is 0 when starts with;%n", rsl4[0]);
         Arrays.stream(rsl4).forEach(out::println);
 
-        /*String [] rsl5 = str5.split(";");
-        out.println(rsl5.length);*/
+        String[] rsl5 = str5.split(";");
+        out.println(rsl5.length);
+
+
+        out.println("this is ;;");
+        String[] rsl6 = str6.split(";", 1);
+        out.println(rsl6.length);
+        out.println(rsl6[0].isBlank());
+        out.println(rsl6[1].isBlank());
+        out.println(rsl6[0].length());
+
+        out.printf("this [%s] is 0 when starts with;%n", rsl6[0]);
+        Arrays.stream(rsl6).forEach(out::println);
     }
 }
