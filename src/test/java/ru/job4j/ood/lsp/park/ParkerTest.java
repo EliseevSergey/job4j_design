e314e404dc273e9f00db9e6d02f179a2388d9e9c
EliseevSergey <s.eliseev1@gmail.com>
2023-07-18 14:11:12 +0300

    2. Парковка машин [#853 # [#853 #429828]]

--- src/test/java/ru/job4j/ood/lsp/park/ParkerTest.java
+++ src/test/java/ru/job4j/ood/lsp/park/ParkerTest.java
@@ -30,18 +30,11 @@ class ParkerTest {
     @Test
     void whenUnparkAllCheckContentAndStatus() {
         parker.park(in);
-        /*for (Slot sl : parking.getSlots()) {
-            System.out.printf("%s.  TruckSlotType is %s %n", sl.getContent(), sl.getTruckType());
-        }*/
         assertEquals(true, parker.unPark(in));
-        /*for (Slot sl : parking.getSlots()) {
-            System.out.printf("%s.  TruckSlotType is %s %n", sl.getContent(), sl.getTruckType());
-        }*/
         assertEquals(null, parking.getSlots().get(0).getContent());
         assertEquals(null, parking.getSlots().get(1).getContent());
         assertEquals(null, parking.getSlots().get(2).getContent());
         assertEquals(null, parking.getSlots().get(3).getContent());
-
         assertEquals(false, in.get(0).getParkStatus());
         assertEquals(false, in.get(3).getParkStatus());
         assertEquals(2, parking.getFreeCarSlotQty());
@@ -51,9 +44,6 @@ class ParkerTest {
     @Test
     void findIndex() {
         parker.park(in);
-        for (Slot sl : parking.getSlots()) {
-            System.out.printf("%s.  TruckSlotType is %s %n", sl.getContent(), sl.getTruckType());
-        }
         assertEquals(3, parking.indexOfVehicle(secondCar).get(0));
         assertEquals(2, parking.indexOfVehicle(firstCar).get(0));
         assertEquals(1, parking.indexOfVehicle(secondTruck).get(0));
@@ -70,12 +60,8 @@ class ParkerTest {
         assertEquals(true, parker.park(input));
         List<Integer> expectedForTruckOnCarSlots = List.of(2, 3, 4, 5, 6);
         List<Integer> expectedForTailCar = List.of(7);
-        /*for (Slot sl : park.getSlots()) {
-            System.out.printf("%s.  TruckSlotType is %s %n", sl.getContent(), sl.getTruckType());
-        }*/
         assertEquals(expectedForTruckOnCarSlots, park.indexOfVehicle(truckForCarSlots));
         assertEquals(expectedForTailCar, park.indexOfVehicle(tailCar));
-
     }
 
     @Test
@@ -101,12 +87,8 @@ class ParkerTest {
         Parker parker = new Parker(park);
         Truck truckForCarSpace = new Truck(2, "TFCS");
         Truck truckForCarSpaceTail = new Truck(3, "TFCST");
-        List<Vehicle> input =
-                List.of(firstCar, firstTruck, secondCar, truckForCarSpace, thirdCar, truckForCarSpaceTail);
+        List<Vehicle> input = List.of(firstCar, firstTruck, secondCar, truckForCarSpace, thirdCar, truckForCarSpaceTail);
         assertEquals(true, parker.park(input));
-        for (Slot sl : park.getSlots()) {
-            System.out.printf("%s.  TruckSlotType is %s %n", sl.getContent(), sl.getTruckType());
-        }
         assertEquals(2, park.getFreeCarSlotQty());
         assertEquals(0, park.getFreeTruckSlotQty());
         assertEquals(firstTruck, park.getSlots().get(0).getContent());
@@ -120,16 +102,6 @@ class ParkerTest {
         assertEquals(truckForCarSpaceTail, park.getSlots().get(8).getContent());
         assertEquals(null, park.getSlots().get(9).getContent());
         assertEquals(null, park.getSlots().get(10).getContent());
-        List<Vehicle> out = List.of(truckForCarSpace);
-        System.out.println(parking.freeTruckSlotQty);
-        System.out.println(parking.freeCarSlotQty);
-        parker.unPark(out);
-        System.out.println("AFTER");
-        for (Slot sl : park.getSlots()) {
-            System.out.printf("%s.  TruckSlotType is %s %n", sl.getContent(), sl.getTruckType());
-        }
-        System.out.println(parking.freeTruckSlotQty);
-        System.out.println(parking.freeCarSlotQty);
     }
 
     @Test
